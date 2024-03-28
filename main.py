2c8b1aaa624f68d61bf4cd834435464ebf28aef5
hmaxlamb <h.max.lamb@gmail.com>
2024-03-28 18:32:00 -0400

    added a wordcount function in main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world")
+def main():
+    book_path = "books/frankenstein.txt"
+    book_text = get_book_text(book_path)
+    word_count = get_num_words(book_text)
+    print(f"There are {word_count} words found")
+
+
+
+def get_num_words(text):
+    words = text.split()
+    count = len(words)
+    return count
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
